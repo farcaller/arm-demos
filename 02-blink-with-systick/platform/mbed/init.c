adfaa717c5e0cdbb9f62782ae670a1afd780d295
Vladimir Pouzanov <farcaller@gmail.com>
2013-08-13 22:00:28 +0100

    Added a stub platform_init for mbed

---
+++ 02-blink-with-systick/platform/mbed/init.c
@@ -0,0 +1,12 @@
+#include "LPC17xx.h"
+
+#define CLOCK_MODE_IRC          0 // 4MHz
+#define CLOCK_MODE_IRC_WITH_PLL 1 // ? MHz
+#define CLOCK_MODE_SYS_WITH_PLL 2 // ? MHz
+
+#define CLOCK_MODE CLOCK_MODE_IRC
+
+void platform_init()
+{
+
+}
