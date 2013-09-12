d22bde1db61e86d41d2ab7ff775d1121836a770e
Vladimir Pouzanov <farcaller@gmail.com>
2013-09-12 21:05:46 +0100

    Blink in a happier pattern

--- 04-hello-world/app/helloworld.c
+++ 04-hello-world/app/helloworld.c
@@ -9,9 +9,9 @@ void setup()
 void loop()
 {
     platform_led_toggle(1);
-    platform_delay(1000);
-    platform_led_toggle(0);
     platform_delay(500);
+    platform_led_toggle(0);
+    platform_delay(1000);
 
     //platform_delay(1000);
     //platform_uart_writef("hello world\n");
