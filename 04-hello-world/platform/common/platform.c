64573531ac08ca66ce3f33bf26a14cae0a337090
Vladimir Pouzanov <farcaller@gmail.com>
2013-09-17 17:52:24 +0100

    Should be a bit more compact

--- 04-hello-world/platform/common/platform.c
+++ 04-hello-world/platform/common/platform.c
@@ -24,19 +24,12 @@ void main()
 void platform_init_data()
 {
     uint32_t *load_addr = &_data_load;
-    uint32_t *mem_addr = &_data;
-    uint32_t *end_addr = &_edata;
-    uint32_t len = (uint8_t *)end_addr - (uint8_t *)mem_addr;
 
-    for (uint32_t i = 0; i < len; i += sizeof(load_addr)) {
+    for (uint32_t *mem_addr = &_data; mem_addr < &_edata; ) {
         *mem_addr++ = *load_addr++;
     }
 
-    mem_addr = &_bss;
-    end_addr = &_ebss;
-    len = (uint8_t *)end_addr - (uint8_t *)mem_addr;
-
-    for (uint32_t i = 0; i < len; i += sizeof(load_addr)) {
+    for (uint32_t *mem_addr = &_bss; mem_addr < &_ebss; ) {
         *mem_addr++ = 0;
     }
 }
